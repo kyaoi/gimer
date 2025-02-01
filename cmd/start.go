b7a85f619ae9393215b146ab9e3f1ccab4f1d79e
kyaoi <aoistudy90@gmail.com>
2025-02-01 14:51:12 +0900

    feat: display ascii clock

--- cmd/start.go
+++ cmd/start.go
@@ -6,23 +6,28 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"sync/atomic"
 	"time"
 
+	"github.com/common-nighthawk/go-figure"
 	"github.com/faiface/beep"
 	"github.com/faiface/beep/speaker"
 	"github.com/faiface/beep/wav"
 	"github.com/google/uuid"
+	"github.com/jroimartin/gocui"
 	"github.com/spf13/cobra"
 )
 
 var (
-	seconds      int
-	minutes      int
-	hours        int
-	description  string
-	saveFlag     bool
+	seconds          int
+	minutes          int
+	hours            int
+	description      string
+	saveFlag         bool
 	useSaveTimerFlag bool
 )
+
+var running int32 = 1 // カウントダウンの実行状態 (1: 実行中, 0: 停止中)
 
 var startCmd = &cobra.Command{
 	Use:   "start",
@@ -144,29 +149,93 @@ var startCmd = &cobra.Command{
 			}
 		}
 
-		fmt.Printf("Timer started: Description=%s, Duration=%s\n", description, duration)
-
 		runTimer(timer)
 	},
 }
 
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	v, err := g.SetView("timer", maxX/2-35, maxY/2-9, maxX/2+35, maxY/2+9)
+	if err != nil && err != gocui.ErrUnknownView {
+		return err
+	}
+	v.Frame = false
+	return nil
+}
+
+func displayTime(g *gocui.Gui, t *ActiveTimer, d time.Duration) {
+	g.Update(func(g *gocui.Gui) error {
+		v, err := g.View("timer")
+		if err != nil {
+			return err
+		}
+		v.Clear()
+		timeStr := fmt.Sprintf("%02d:%02d:%02d", int(d.Hours()), int(d.Minutes())%60, int(d.Seconds())%60)
+		fig := figure.NewFigure(timeStr, "big", true)
+		fmt.Fprint(v, fig.String())
+		if err := loadActiveTimerState(); err != nil {
+			return fmt.Errorf("Error loading timer state: %v\n", err)
+		}
+		exists := getActiveTimerById(t.ID)
+		if !exists {
+			return gocui.ErrQuit
+		}
+		return nil
+	})
+}
+
+func updateTimer(g *gocui.Gui, t *ActiveTimer, d time.Duration) {
+	for r := d; r > 0; r -= time.Second {
+		for atomic.LoadInt32(&running) == 0 {
+			time.Sleep(100 * time.Millisecond) // 停止中は待機
+			exists := getActiveTimerById(t.ID)
+			if !exists {
+				os.Exit(0)
+			}
+		}
+		g.Update(func(g *gocui.Gui) error {
+			displayTime(g, t, (r+time.Second-1)/time.Second*time.Second) // 小数秒を切り上げて表示
+			return nil
+		})
+		time.Sleep(1 * time.Second)
+	}
+	displayTime(g, t, 0) // 完全に0になったときに表示を更新
+	playSound(t)         // 0秒になった瞬間にサウンドを再生
+}
+
+func togglePause(g *gocui.Gui, v *gocui.View) error {
+	if atomic.LoadInt32(&running) == 1 {
+		atomic.StoreInt32(&running, 0) // 停止
+	} else {
+		atomic.StoreInt32(&running, 1) // 再開
+	}
+	return nil
+}
+
 func runTimer(timer *ActiveTimer) {
 	duration := time.Until(timer.TriggerTimer)
-	for remaining := duration; remaining > 0; remaining -= time.Second {
-		if err := loadActiveTimerState(); err != nil {
-			fmt.Printf("Error loading timer state: %v\n", err)
-			return
-		}
-		exists := getActiveTimerById(timer.ID)
-		if !exists {
-			fmt.Println("Timer has been deleted")
-			return
-		}
-		time.Sleep(time.Second)
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		panic(err)
+	}
+	defer g.Close()
+
+	g.SetManagerFunc(layout)
+	go updateTimer(g, timer, duration)
+
+	if err := g.SetKeybinding("", gocui.KeySpace, gocui.ModNone, togglePause); err != nil { // Enterキーで開始・停止
+		panic(err)
 	}
 
-	fmt.Printf("Timer ended: Description=%s\n", timer.Description)
-	playSound(timer)
+	if err := g.SetKeybinding("", 'q', gocui.ModNone, func(g *gocui.Gui, v *gocui.View) error {
+		return gocui.ErrQuit
+	}); err != nil {
+		panic(err)
+	}
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		panic(err)
+	}
 }
 
 func playSound(timer *ActiveTimer) {
--- go.mod
+++ go.mod
@@ -3,14 +3,18 @@ module github.com/kyaoi/gimer
 go 1.23.2
 
 require (
+	github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be
 	github.com/faiface/beep v1.1.0
 	github.com/google/uuid v1.6.0
+	github.com/jroimartin/gocui v0.5.0
 	github.com/spf13/cobra v1.8.1
 )
 
 require (
 	github.com/hajimehoshi/oto v0.7.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/nsf/termbox-go v1.1.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/exp v0.0.0-20190306152737-a1d7652674e8 // indirect
